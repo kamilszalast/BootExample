8f35f8c07273020d69f5a2c207b779d9ec3e477b
kamil <kamilszalast0@gmail.com>
2020-02-28 14:22:54 +0100

    20.02.2020 First Advanced Task

--- src/main/java/org/example/collection/advance/CollectionsAdvanceExample.java
+++ src/main/java/org/example/collection/advance/CollectionsAdvanceExample.java
@@ -1,14 +1,31 @@
 package org.example.collection.advance;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.example.BootExampleUtils;
 import org.example.dto.Mall;
 import org.example.dto.Product;
 import org.example.dto.Store;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class CollectionsAdvanceExample implements CollectionsAdvanceExampleInterface {
 
     @Override
     public Product getTheMostAvailableProductFromStore(Store store) {
-        return null;
+            return store.getProducts().stream()
+                    .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                    .entrySet()
+                    .stream()
+                    .max(Comparator.comparing(Map.Entry::getValue))
+                    .map(Map.Entry::getKey)
+                    .get();
+
+
     }
 
     @Override
